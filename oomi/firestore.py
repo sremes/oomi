3ce47bbf6f319069fe0217042fed75f777fb296b
Sami Remes <sami.remes@gmail.com>
2021-01-22 22:07:19 +0200

    Handle datetimes in firestore

--- oomi/firestore.py
+++ oomi/firestore.py
@@ -1,4 +1,7 @@
 """Implement firestore database interface."""
+from datetime import datetime
+import pytz
+
 from google.cloud import firestore
 import pandas as pd
 
@@ -9,6 +12,7 @@ class FirestoreConfig:  # pylint: disable=too-few-public-methods
     """Basic configuration of the database."""
 
     COLLECTION = "oomi"  # collection to save daily consumption docs
+    TIMEZONE = pytz.timezone("Europe/Helsinki")
 
 
 class Firestore(Database):
@@ -16,28 +20,36 @@ class Firestore(Database):
 
     def __init__(self, config: FirestoreConfig, project=None, credentials=None) -> None:
         """Create the firestore client instance."""
-        # Project ID is determined by the GCLOUD_PROJECT environment variable
         self.client = firestore.Client(project=project, credentials=credentials)
         self.config = config
 
     def upload_data(self, data: pd.DataFrame) -> None:
         """Upload dataframe to database."""
         for date, consumption in zip(data["date"], data["consumption"]):
-            self.client.collection(self.config.COLLECTION).document(date).set({"date": date, "consumption": consumption})
+            self.client.collection(self.config.COLLECTION).document(f"{date:%Y-%m-%d}").set(
+                {"date": date, "consumption": consumption}
+            )
 
     def download_data(self) -> pd.DataFrame:
         """Download data from database."""
-        return pd.DataFrame.from_records(
-            [doc.to_dict() for doc in self.client.collection(self.config.COLLECTION).stream()]
-        )
+        docs = [doc.to_dict() for doc in self.client.collection(self.config.COLLECTION).stream()]
+        dataframe = pd.DataFrame.from_records(docs)
+        dataframe["date"] = dataframe["date"].dt.tz_convert(self.config.TIMEZONE)
+        return dataframe
 
 
 def main():
     """Run a simple test with firestore."""
     config = FirestoreConfig()
-    config.COLLECTION = "test"
+    config.COLLECTION = "test"  # pylint: disable=invalid-name
     client = Firestore(config=config)
-    data = pd.DataFrame.from_records([{"date": "monday", "consumption": 69}, {"date": "tuesday", "consumption": 42}])
+    tz = config.TIMEZONE
+    data = pd.DataFrame.from_records(
+        [
+            {"date": tz.localize(datetime(2021, 1, 1)), "consumption": 69},
+            {"date": tz.localize(datetime(2021, 1, 2)), "consumption": 42},
+        ]
+    )
     client.upload_data(data)
     retrieved = client.download_data()
     print(retrieved)
