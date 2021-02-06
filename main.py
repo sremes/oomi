c715fc1f68cc8a9372d1f7c79d59ec151afd9180
Sami Remes <sami.remes@gmail.com>
2021-02-06 12:27:51 +0200

    Parse timestamp to get latest data

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 """Main function for running as a Google Cloud Function."""
 
+from base64 import b64decode
+from datetime import datetime, timedelta
 import os
+from typing import Dict
 
 from oomi.firestore import Firestore, FirestoreConfig
 from oomi.oomi_downloader import OomiDownloader, OomiConfig
 
 
-def update_firestore(event: dict, context: dict) -> None:
+def update_firestore(event: Dict[str, str], context) -> None:
     """Update Cloud Function to be triggered by Pub/Sub.
     Args:
          event (dict):  The dictionary with data specific to this type of
@@ -18,15 +21,26 @@ def update_firestore(event: dict, context: dict) -> None:
     """
     print(f"This Function was triggered by messageId {context.event_id} published at {context.timestamp}")
 
-    print(f"Event: {event}")
+    if "data" in event:
+        data = b64decode(event["data"]).decode("utf-8")
+        print(f"Event data: {data}")
 
-    if False is None:
-        # get data
-        downloader = OomiDownloader(
-            config=OomiConfig(), username=os.environ.get("OOMI_USER"), password=os.environ.get("OOMI_PASSWORD")
-        )
-        data_frame = downloader.get_consumption("2021-01-01", "2021-01-02")
+    # get dates from the current timestamp
+    assert context.timestamp.endswith("Z"), "Timestamp has to be in UTC"
+    firestore_config = FirestoreConfig()
+    timestamp = datetime.fromisoformat(context.timestamp.replace("Z", ""))
+    timestamp = firestore_config.TIMEZONE.fromutc(timestamp)
 
-        # upload
-        client = Firestore(config=FirestoreConfig())
-        client.upload_data(data_frame)
+    first_date = timestamp - timedelta(days=2)  # data is available with 2 days delay
+    last_date = timestamp - timedelta(days=1)
+
+    # get data
+    print(f"Getting consumption data from: {first_date:%Y-%m-%d}")
+    downloader = OomiDownloader(
+        config=OomiConfig(), username=os.environ.get("OOMI_USER"), password=os.environ.get("OOMI_PASSWORD")
+    )
+    data_frame = downloader.get_consumption(f"{first_date:%Y-%m-%d}", f"{last_date:%Y-%m-%d}")
+
+    # upload
+    client = Firestore(config=firestore_config)
+    client.upload_data(data_frame)
